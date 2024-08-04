d66f968f9092565b494e072f351764e1bdb9961e
24onsi <24onsi@gmail.com>
2024-08-04 21:26:46 +0900

    컴퓨터 리소스 모니터링

---
+++ 코드파일/ResourceMonitoring/MainWindow.xaml.cs
@@ -0,0 +1,282 @@
+﻿using LiveCharts.Wpf;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using Google.Protobuf.WellKnownTypes;
+using LiveCharts;
+using System.Runtime.CompilerServices;
+using System.Collections.Concurrent;
+using static System.Net.Mime.MediaTypeNames;
+using System.Threading;
+using System.Windows.Threading;
+using System;
+using Microsoft.Win32;
+
+namespace ResourceMonitoring
+{
+    public partial class MainWindow : Window
+    {
+        private System.Timers.Timer DB_timer;
+
+        private System.Timers.Timer Error_timer;
+
+        private CPULineChart cpuchartUpdate;
+        private static double cpuValue { get; set; }
+        private static double memValue { get; set; }
+        private static double diskValue { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cpuchartUpdate = new CPULineChart();
+            DataContext = cpuchartUpdate;
+
+            Thread DBthread = new Thread(new ThreadStart(BackThread));
+            DBthread.IsBackground = true;
+            DBthread.Start();
+        }
+
+        public void UI_Start()
+        {
+            CpuChartValue();
+
+            MemoryChartValue();
+
+            DiskChartValue();
+        }
+
+        public void CpuChartValue()
+        {
+            Task.Run(async() =>
+            {
+                while (true)
+                {
+                    cpuValue = Math.Ceiling(GetCpuValue());
+
+                    await Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        cpu_graph.Value = cpuValue;
+                        cpuchartUpdate.CpuValues.Add(cpuValue);
+                        if (cpuchartUpdate.CpuValues.Count > 20)
+                            cpuchartUpdate.CpuValues.RemoveAt(0);
+                    }));
+                    await Task.Delay(1000);
+                }
+            });
+        }
+
+        public void MemoryChartValue()
+        {
+            Task.Run(async() =>
+            {
+                while (true)
+                {
+                    memValue = Math.Ceiling(GetMemValue());
+
+                    await Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        memory_graph.Value = memValue;
+                    }));
+                    await Task.Delay(1000);
+                }
+            });
+        }
+
+        public void DiskChartValue()
+        {
+            Task.Run(async() =>
+            {
+                while (true)
+                {
+                    diskValue = Math.Ceiling(GetDiskValue());
+
+                    await Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        disk_graph.Value = diskValue;
+                    }));
+                    await Task.Delay(1000);
+                }
+            });
+        }
+
+        public static double GetCpuValue()
+        {
+            var CpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            CpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            double returnvalue = (double)CpuCounter.NextValue();
+
+            return returnvalue;
+        }
+
+        public static double GetMemValue()
+        {
+            var MemCounter = new PerformanceCounter("Memory", "% Committed Bytes in Use");
+            double returnvalue = (double)MemCounter.NextValue();
+
+            return returnvalue;
+        }
+
+        public static double GetDiskValue()
+        {
+            {
+                double TotalDiskSize = 0;
+                double AvailableDiskSize = 0;
+                double UsedDiskSize = 0;
+                double UsedDiskSizePercent = 0;
+                DriveInfo[] drives = DriveInfo.GetDrives();
+
+                foreach (DriveInfo drive in drives)
+                {
+                    if (drive.IsReady)
+                    {
+                        TotalDiskSize += drive.TotalSize;
+                        AvailableDiskSize += drive.AvailableFreeSpace;
+                    }
+                }
+                UsedDiskSize = TotalDiskSize - AvailableDiskSize;
+                UsedDiskSizePercent = (UsedDiskSize / TotalDiskSize) * 100;
+
+                return UsedDiskSizePercent;
+            }
+        }
+
+        public void BackThread()
+        {
+            DB_timer = new System.Timers.Timer();
+            DB_timer.Interval = 60000;
+            DB_timer.Elapsed += InsertDB;
+            DB_timer.AutoReset = true;
+            DB_timer.Enabled = true;
+
+            Error_timer = new System.Timers.Timer();
+            Error_timer.Interval = 5000;
+            Error_timer.Elapsed += ErrorCheck;
+            Error_timer.AutoReset = true;
+            Error_timer.Enabled = true;
+        }
+
+        public void InsertDB(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            MySqlConnection conn;
+            string connectString = "Server=127.0.0.1;Port=3306;Database=monitoring;Uid=sion;Pwd=1234;CharSet=utf8;";
+
+            try
+            {
+                conn = new MySqlConnection(connectString);
+                conn.Open();
+                conn.Ping();
+                string cpu = Math.Round(cpuValue).ToString();
+                string mem = Math.Round(memValue).ToString();
+                string disk = Math.Round(diskValue).ToString();
+
+                if (cpu == "0" || mem == "0" || disk == "0")
+                {
+                    conn.Close();
+                }
+                else
+                {
+                    string query = "INSERT INTO RESOURCE VALUES(DEFAULT," + cpu + "," + mem + "," + disk + ");";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+
+        private void btn_search_Click(object sender, RoutedEventArgs e)
+        {
+            search SearchWindow = new search();
+            SearchWindow.Show();
+        }
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            UI_Start();
+        }
+
+        public void ErrorCheck(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            string cpu = Math.Round(cpuValue).ToString();
+            string mem = Math.Round(memValue).ToString();
+            string disk = Math.Round(diskValue).ToString();
+
+            if (cpu == "0" || mem == "0" || disk == "0")
+            {
+                ErrorFileCreate(cpu, mem, disk);
+            }
+        }
+
+        public void ErrorFileCreate(string error_cpu, string error_mem, string error_disk)
+        {
+            try
+            {
+                string time_error = DateTime.Now.ToString("yyyy년MM월dd일HH시mm분ss초");
+                string CreateFile = @"C:\Users\uy122\OneDrive\바탕 화면\ErrorFile\errorfile" + time_error + ".txt";
+                StreamWriter textWrite = File.CreateText(CreateFile);
+
+                string time = "시간 : " + time_error;
+                string cpu_error = "\nCPU 사용량 : " + error_cpu;
+                string memory_error = "\n메모리 사용량 : " + error_mem;
+                string disk_error = "\n디스크 사용량 : " + error_disk;
+                string write_text = time + cpu_error + memory_error + disk_error;
+
+                textWrite.WriteLine(write_text); //쓰기
+                textWrite.Dispose(); //파일 닫기
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+
+    public class CPULineChart : INotifyPropertyChanged
+    {
+        private ChartValues<double> _cpuValues;
+
+        public ChartValues<double> CpuValues
+        {
+            get { return _cpuValues; }
+            set
+            {
+                _cpuValues = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CPULineChart()
+        {
+            _cpuValues = new ChartValues<double>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ 코드파일/ResourceMonitoring/search.xaml.cs
@@ -0,0 +1,162 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.RightsManagement;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ResourceMonitoring
+{
+    public partial class search : Window
+    {
+        public static List<string> DateTimeList = new List<string>();
+        public SeriesCollection SeriesData { get; private set; }
+        public List<string> XLabel { get; set; }
+        public Func<int, string> Values { get; set; }
+        public ListView ListDate { get; set; }
+
+        public search()
+        {
+            InitializeComponent();
+            ComboListInit();
+        }
+
+        public void createLinechart()
+        {
+            SeriesData = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "CPU 사용량",
+                    Values = new ChartValues<int>()
+                },
+                new LineSeries
+                {
+                    Title = "Memory 사용량",
+                    Values = new ChartValues<int>()
+                },
+                new LineSeries
+                {
+                    Title = "Disk 사용량",
+                    Values = new ChartValues<int>()
+                }
+            };
+
+            SearchChart.Series = SeriesData;
+            XLabel = new List<string>();
+            Values = value => value.ToString("N");
+            DataContext = this;
+        }
+
+        public string SearchDateTime { get; set; } 
+        public async Task ComboListInit()
+        {
+            try
+            {
+                MySqlConnection conn1
+            = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=monitoring;Uid=sion;Pwd=1234;CharSet=utf8;");
+                conn1.Open();
+                conn1.Ping();
+
+                string query = "SELECT LEFT(TIME, 13) FROM RESOURCE WHERE TIME GROUP BY LEFT(TIME, 13);";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn1);
+                MySqlDataReader? dr = cmd.ExecuteReader();
+
+                DateTimeList.Clear();
+
+                while (await dr.ReadAsync())
+                {
+                    DateTimeList.Add($"{(dr[0].ToString())}시");
+                }
+
+                dr.Close();
+                conn1.Close();
+
+                await Dispatcher.BeginInvoke(() =>
+                {
+                    time_combo.ItemsSource = DateTimeList;
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("쿼리 오류: " + ex.Message);
+            }
+        }
+
+        public async Task SearchData(string date)
+        {
+            SearchDateTime = date;
+            date = date.Remove(13);
+            try
+            {
+                MySqlConnection conn2
+            = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=monitoring;Uid=sion;Pwd=1234;CharSet=utf8;");
+                conn2.Open();
+                conn2.Ping();
+
+                string query = $"SELECT * FROM RESOURCE WHERE TIME LIKE '{date}%' ORDER BY TIME;";
+
+                MySqlCommand cmd = new MySqlCommand(query, conn2);
+                MySqlDataReader? dr = cmd.ExecuteReader();
+
+                await Dispatcher.BeginInvoke(() =>
+                {
+                    SeriesData[0].Values.Clear();
+                    SeriesData[1].Values.Clear();
+                    SeriesData[2].Values.Clear();
+                    SearchChart.AxisX.Clear();
+
+                    XLabel.Clear();
+                });
+
+                while (await dr.ReadAsync())
+                {
+                    string SearchDate = dr[0].ToString();
+                    SearchDate = SearchDate.Substring(14);
+
+                    await Dispatcher.BeginInvoke(() =>
+                    {
+                        XLabel.Add(SearchDate);
+                        SeriesData[0].Values.Add(Convert.ToInt32(dr[1]));
+                        SeriesData[1].Values.Add(Convert.ToInt32(dr[2]));
+                        SeriesData[2].Values.Add(Convert.ToInt32(dr[3]));
+                    });
+                }
+
+                await Dispatcher.BeginInvoke(() =>
+                {
+                    
+                    SearchChart.AxisX.Add(new Axis
+                    {
+                        Title = "시간대",
+                        Labels = XLabel
+                    });
+                });
+
+                dr.Close();
+                conn2.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("쿼리 오류: " + ex.Message);
+            }
+        }
+
+        public async void btn_result_Click(object sender, RoutedEventArgs e)
+        {
+            if (time_combo.SelectedItem != null)
+            {
+                string date = time_combo.SelectedItem.ToString();
+                lbl_date.Content = date + " 리소스 모니터링 데이터 검색";
+                createLinechart();
+                await SearchData(date);
+            }
+        }
+    }
+}
